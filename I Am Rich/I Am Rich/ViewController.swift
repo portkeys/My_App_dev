9880314095ec942defb0937d7fb6c6e049483399
Wen Yang <wyang@livongo.com>
2021-01-11 23:21:08 -0800

    Add 3 apps: rich todolist and meal tracker

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Wen Yang on 1/9/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
